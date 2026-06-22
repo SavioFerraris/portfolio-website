90b41f4ac52f11d922be42b5fff76feec4edbcad
SavioFerraris <Savio.Ferraris@student.pxl.be>
2026-06-22 19:09:33 +0200

    responsive design

--- global/hamburger.js
+++ global/hamburger.js
@@ -1,5 +1,19 @@
-function toggleMenu() {
+document.addEventListener("DOMContentLoaded", () => {
+
     const nav = document.querySelector(".nav-list");
-    nav.classList.toggle("active");
-}
-``
+    const hamburger = document.querySelector(".hamburger");
+
+    function toggleMenu() {
+        nav.classList.toggle("active");
+    }
+
+    // maak functie globaal zodat HTML onclick werkt
+    window.toggleMenu = toggleMenu;
+
+    document.addEventListener("click", (e) => {
+        if (!nav.contains(e.target) && !hamburger.contains(e.target)) {
+            nav.classList.remove("active");
+        }
+    });
+
+});
